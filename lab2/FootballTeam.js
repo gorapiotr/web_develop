b0b4720dd7e7407e9712695f0c55c8109f1b551c
gorapiotr <pgora@sigma.ug.edu.pl>
2018-03-17 09:35:44 +0100

    lab2
    lab2

--- lab2/FootballTeam.js
+++ lab2/FootballTeam.js
@@ -1,32 +1,90 @@
+var lib = {};
 
-
-var FootballTeam = (function () {
+ lib.footballTeam = (function () {
 
     var name = "Liverpool";
     var country = "England";
     var leauge = "Premier League";
-    var players = ["Mane", "Firmino", "van Dijk", "Henderson", "Milner", "Loveren", "Moreno", "Karius"];
 
-    
+    var fieldPositions = {
+    goalkeeper: 0,
+    defender: 1,
+    miedfielder: 2,
+    forward: 3
+    };
+
+    var players = [
+        {
+            name:"John",
+            surname:"Mane",
+            fieldPosition: fieldPositions.goalkeeper 
+        },
+        {  
+            name:"Tom",
+            surname:"Firmino",
+            fieldPosition: fieldPositions.goalkeeper 
+        },
+        {  
+            name:"Adrian",
+            surname:"Firmino",
+            fieldPosition: fieldPositions.defender 
+        },
+        {  
+            name:"James",
+            surname:"Henderson",
+            fieldPosition: fieldPositions.miedfielder
+        },
+        {  
+            name:"John",
+            surname:"Salah",
+            fieldPosition: fieldPositions.forward
+        },
+        {  
+            name:"Robert",
+            surname:"Moreno",
+            fieldPosition: fieldPositions.goalkeeper 
+        },
+
+    ];
+
 
     var amountPlayers = function(){
-        return this.players.length;
+        return players.length;
     };
 
     var getThreeFirstLettersTeamName = function(){
-        return this.name[0].toUpperCase()+this.name[1].toUpperCase()+this.name[2].toUpperCase();
+        return name[0].toUpperCase()+name[1].toUpperCase()+name[2].toUpperCase();
     };
 
-    var getUppercaseName =  function(){
-        return this.name.toUpperCase();
+    var getUpperCaseName =  function(){
+        return name.toUpperCase();
     };
+
+    var howManyGoalkeepers = function(){
+
+        var counter = 0;
+
+        players.filter(function(player){
+            if(player.fieldPosition === fieldPositions.goalkeeper)
+            {
+                counter++;
+            }
+        });
+        return counter;
+    }
 
     return{
         players: players,
         amountPlayers: amountPlayers,
         getThreeFirstLettersTeamName: getThreeFirstLettersTeamName,
-        getUppercaseName: getUppercaseName
+        getUpperCaseName: getUpperCaseName,
+        howManyGoalkeepers: howManyGoalkeepers
     }
 
 })();
 
+
+
+console.log(lib.footballTeam.amountPlayers());
+
+console.log(lib.footballTeam.howManyGoalkeepers());
---
+++ lab3/lab3.js
@@ -0,0 +1,12 @@
+//Modelowanie klas///
+
+var types = {
+    ele: 0,
+    tele: 1
+};
+
+var tools = [{name: "Hammer", type: [types.ele,types.tele]},{},{}];
+
+console.log(tools[0]);
+
+
